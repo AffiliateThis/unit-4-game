bf4881ddc8d8013e7f5bd7bc584a929f6e1daaac
Affiliatethis <chris@onecrookedtree.com>
2019-03-09 14:32:40 -0500

    4th update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,67 +11,68 @@ $(document).ready(function() {
   var button4;
   var randomUserNum;
   var userScore = 0;
+  var wins = 0;
+  var losses = 0;
 
   function gameStart() {
     randomUserNum = Math.floor(Math.random() * 101) + 20;
     $("#randomusernum").text(randomUserNum);
+    userScore = 0;
     $("#userscore").text(userScore);
-    console.log(randomUserNum);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 
     button1 = Math.floor(Math.random() * 12) + 1;
-    console.log(button1);
 
     button2 = Math.floor(Math.random() * 12) + 1;
-    console.log(button2);
 
     button3 = Math.floor(Math.random() * 12) + 1;
-    console.log(button3);
 
     button4 = Math.floor(Math.random() * 12) + 1;
-    console.log(button4);
   }
   // $(userScore).math(button1 + button2);
 
   $("#button1").click(function() {
     userScore = userScore + button1;
     $("#userscore").text(userScore);
-    console.log(userScore);
-    console.log(randomUserNum);
+
+    checkWinlose();
     // add the click event value here per click event
   });
 
   $("#button2").click(function() {
     userScore = userScore + button2;
     $("#userscore").text(userScore);
-    console.log(userScore);
-    console.log(randomUserNum);
+
     checkWinlose();
   });
 
   $("#button3").click(function() {
     userScore = userScore + button3;
     $("#userscore").text(userScore);
-    console.log(userScore);
-    console.log(randomUserNum);
+
     checkWinlose();
   });
 
   $("#button4").click(function() {
     userScore = userScore + button4;
     $("#userscore").text(userScore);
-    console.log(userScore);
-    console.log(randomUserNum);
+
     checkWinlose();
   });
 
   function checkWinlose() {
     if (userScore === randomUserNum) {
       alert("You win!");
+      wins++;
       gameStart();
     } else if (userScore > randomUserNum) {
       alert("You lose Loser!");
+      losses++;
       gameStart();
     }
+    // fuction wins/losses
   }
+
   gameStart();
 });
